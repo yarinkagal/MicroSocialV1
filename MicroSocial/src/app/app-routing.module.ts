5f509b879271c67c96160d9108ca34d6f4507ce7
Ravit Kodman <ravitkodman@microsoft.com>
2022-09-19 11:35:55 +0300

    Add first draft to preferences routing and component

--- MicroSocial/src/app/app-routing.module.ts
+++ MicroSocial/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PreferencesComponent } from './preferences/preferences.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'preferences',component: PreferencesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
