1833c4728b7a6f34960322f78bb00664bc5c4655
meytalmicrosoft <meytal.kozlovsky@microsoft.com>
2022-09-19 11:08:12 +0200

    sign in page

--- MicroSocial/src/app/app-routing.module.ts
+++ MicroSocial/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { SignInComponent } from './sign-in/sign-in.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component:AppComponent },
+  { path: 'sign-in', component:SignInComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
